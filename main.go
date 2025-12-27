7e45a94bca4be6ae6abe266e8d99b024904286f1
Tian Istenič <tian.istenic34@gmail.com>
2025-12-27 21:24:05 +0100

    feat: added rate limiter

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"main.go/internal/db"
@@ -8,21 +11,42 @@ import (
 	"main.go/internal/middleware"
 )
 
+func keyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
+
+func errorHandler(c *gin.Context, info ratelimit.Info) {
+	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
+}
+
 func main() {
 	godotenv.Load()
 	db.InitDB()
 	defer db.DB.Close()
 	router := gin.Default()
-	authorized := router.Group("/users")
-	authorized.Use(middleware.AuthMiddleware())
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate:  time.Second,
+		Limit: 5,
+	})
+	rateLimiter := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: errorHandler,
+		KeyFunc:      keyFunc,
+	})
+	api := router.Group("/api")
+	api.Use(rateLimiter)
 	{
-		authorized.GET("todos", handlers.GetTodos)
-		authorized.POST("todos", handlers.AddTodo)
-		authorized.DELETE("todos/:todoId", handlers.DeleteTodo)
-		authorized.PUT("todos/:todoId", handlers.UpdateTodo)
+
+		authorized := api.Group("/users")
+		authorized.Use(middleware.AuthMiddleware())
+		{
+			authorized.GET("/todos", handlers.GetTodos)
+			authorized.POST("/todos", handlers.AddTodo)
+			authorized.DELETE("/todos/:todoId", handlers.DeleteTodo)
+			authorized.PUT("/todos/:todoId", handlers.UpdateTodo)
+		}
+		api.GET("/ping", handlers.Ping)
+		api.POST("/register", handlers.Register)
+		api.POST("/login", handlers.Login)
 	}
-	router.GET("/ping", handlers.Ping)
-	router.POST("/register", handlers.Register)
-	router.POST("/login", handlers.Login)
 	router.Run()
 }
